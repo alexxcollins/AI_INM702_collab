bebde7135a725c2dad3949d67e1b954c7523da84
Alex Collins <alexxcollins@googlemail.com>
2021-11-17 20:17:45 +0000

    added commment to Outlier.py

--- src/Outlier.py
+++ src/Outlier.py
@@ -11,6 +11,12 @@ from regression import GenerateData, UniformX
 
 class Outlier(UniformX):
     
+    # Alex: I changed the class Outlier inherits from to UniformX, so that
+    # you can generate X data from uniform distribution
+    # the problem with that is that "generate_X" below shares the same name,
+    # so the uniform X data can't be generated. I think if name of method 
+    # below is changed it should all work
+    
     def generate_X(self, original_X, original_y, original_beta, positions, magnitude):
         self.X = np.vstack((original_X,np.array((positions))))
 
