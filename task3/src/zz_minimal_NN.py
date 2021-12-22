3797e2a8efca2d19a44baa857e14f0a4c2f16eef
Alex Collins <alexxcollins@googlemail.com>
2021-12-22 11:36:29 +0100

    implemented minibatch SGD on zz_minimal_NN.py and improved comments in code

--- task3/src/zz_minimal_NN.py
+++ task3/src/zz_minimal_NN.py
@@ -9,12 +9,17 @@ I can't work out what's going wrong with our NN.py. This is an attempt to
 create a really simple NN from scratch to see if it's working
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class NN:
     def __init__(
         self, learning_rate=0.001, regularization=None, reg_alpha=None, random_seed=42
     ):
+        """Intialiase NN class. Set learning rate.
+        regularization is None or string. Currenlty L2 and L2 allowed
+        reg_alpha is float in (0,1)
+        """
         self.lr = learning_rate
         self.regularization = regularization
         self.reg_alpha = reg_alpha
@@ -58,20 +63,71 @@ class NN:
         self.nodes.append(nodes)
         self.activations.append(activation)
 
-    def model(self, X_train, y_train, X_test=None, y_test=None, verbose=True):
+    def model(
+        self,
+        X_train,
+        y_train,
+        X_test=None,
+        y_test=None,
+        minibatch_size=None,
+        verbose=True,
+    ):
+        """
+        After the neural net has been initialised and layers added, model()
+        initialises the model parameters, sets meta data for training and
+        optionally prints out model architecture.
+
+        Parameters
+        ----------
+        X_train : np array, shape (number samples, number features)
+            training data
+        y_train : np array, shape (number of samples, number of classes)
+            one-hot encoded labels for training data
+        X_test : np array, shape (number samples, number features)
+            test data
+        y_test : np array, shape (number of samples, number of classes)
+            one-hot encoded labels for test data
+        minibatch_size : integer or None, optional
+            If None then batch SGD is done (i.e. all samples used for each
+            epoch). If set to one then SGD used with weights reset after cost
+            and gradients calculated for each sample.)
+            Minibatches are randomly sampled at each epoch.
+        verbose : Bool, optional
+            If True then prints out model arhitecture. The default is True.
+
+        Returns
+        -------
+        None. Prints out model architecture if verbose=True
+
+        """
         self.X_train = X_train
         self.y_train = y_train
         self.X_test = X_test
         self.y_test = y_test
         self.no_samples = y_train.shape[0]
         self.nodes[0] = X_train.shape[1]
-        self.batch_size = self.no_samples  # not actively setting this yet - maybe
-        # best to do somewhere else
+        if minibatch_size is None:
+            self.minibatch_size = self.no_samples
+            self.num_batches = 1
+        else:
+            self.minibatch_size = minibatch_size
+            # if minibatch size doesn't divide sample size perfectly then
+            self.num_batches = np.ceil(self.no_samples / self.minibatch_size).astype(
+                int
+            )
+
         self.init_params()
         if verbose:
             print("number of samples = {}".format(self.no_samples))
             print("training data has {} features".format(self.nodes[0]))
             print("learning rate = {}".format(self.lr))
+            print(
+                "regularization is {} with parameter {}".format(
+                    self.regularization, self.reg_alpha
+                )
+            )
+            if minibatch_size is not None:
+                print("minibatch size is {}".format(self.minibatch_size))
             for i in range(self.layers):
                 print("\nhidden layer {}:".format(i))
                 print("nodes in previous layer: {}".format(self.nodes[i]))
@@ -83,33 +139,43 @@ class NN:
     def fit(self, epochs=10, verbose=True):
         self.epochs = epochs
         # set self.y for softmax activation function to take as input
+        ##### this needs to be adjusted if doing minibatch
         self.y = self.y_train
         for e in range(epochs):
-            # forward propagation
-            self.fwd_pass(X=self.X_train)
+            # shuffle the data to ensure different minibatches are returned
+            # for each epoch.
+            if self.num_batches > 1:
+                mask = self.rng.permutation(self.no_samples)
+                X_epoch = self.X_train[mask]
+                y_epoch = self.y_train[mask]
+            else:
+                X_epoch = self.X_train
+                y_epoch = self.y_train
 
-            # store loss function output
-            xe_loss, xe_loss_reg = self.xe_loss(X=self.A[self.layers], y=self.y)
-            self.L.append(xe_loss)
-            self.Lreg.append(xe_loss_reg)
+            # loop over batches:
+            for i in range(self.num_batches):
+                X = X_epoch[i * self.minibatch_size : (i + 1) * self.minibatch_size]
+                y = y_epoch[i * self.minibatch_size : (i + 1) * self.minibatch_size]
+                m = y.shape[0]  # this could be less than
+                # self.minibatch_size for last iteration
 
-            # back propogation. Calculate last layer first and then iterate
-            l = self.layers - 1
-            self.dZ[l] = self.softmax(X=self.A[self.layers], gradient=True)
-            self.dW[l] = self.A[l].T @ self.dZ[l] / self.batch_size
-            self.db[l] = self.dZ[l].sum(axis=0, keepdims=True) / self.batch_size
-            self.dA[l] = self.dZ[l] @ self.W[l].T
-            for l in reversed(range(self.layers - 1)):
-                self.dZ[l] = (
-                    self.activate(self.activations[l], X=self.Z[l], gradient=True)
-                    * self.dA[l + 1]
-                )
-                self.dW[l] = (self.A[l].T @ self.dZ[l]) / self.batch_size
-                self.db[l] = self.dZ[l].sum(axis=0) / self.batch_size
-                self.dA[l] = self.dZ[l] @ self.W[l].T
+                # forward propagation
+                self.fwd_pass(X=X)
 
-            # update weights and biases
-            self.update_params()
+                # store loss function output
+                xe_loss, xe_loss_reg = self.xe_loss(X=self.A[self.layers], y=y, m=m)
+                self.L.append(xe_loss)
+                self.Lreg.append(xe_loss_reg)
+
+                # back propogation.
+                for l in reversed(range(self.layers)):
+                    self.dZ[l] = self.de_Z(l, y)
+                    self.dW[l] = self.de_W(l, m)
+                    self.db[l] = self.de_b(l, m)
+                    self.dA[l] = self.de_A(l)
+
+                # update weights and biases
+                self.update_params()
 
             # print training information
             train_pred = self.predict(self.X_train)
@@ -122,12 +188,31 @@ class NN:
             if verbose:
                 print(
                     "epoch {} loss: {:.3f}; train accy: {:.3f}; test accy: {:.3f}".format(
-                        e, self.L[e], train_accuracy, test_accuracy
+                        e, self.L[-1], train_accuracy, test_accuracy
                     )
                 )
 
     def fwd_pass(self, X, train=True):
+        """
+        Run forward pass. Either changes attributes for all layers: Z and A
+        or if running on test data it just returns the final Z and A layers.
+        For final layers, A is an array of size (batch samples, numnber of
+        classes) with probabilities summing to 1 across eac row.
 
+        Parameters
+        ----------
+        X : np array of shape (batch samples, number of features)
+            Input samples
+        train : Boolean, optional
+            Function runds differently depending on whether it is called in
+            training or testing. If training, then all Z and A matirces in
+            network are updated. The default is True.
+
+        Returns
+        -------
+        A : np array. Shape (number of samples in batch, 10
+        Z : np array. Shape (number of samples in batch, 1)
+        """
         if train:
             self.A[0] = X
             for l in range(self.layers):
@@ -142,6 +227,7 @@ class NN:
             for l in range(self.layers):
                 Z[l] = A[l] @ self.W[l] + self.b[l]
                 A[l + 1] = self.activate(self.activations[l], X=Z[l], gradient=False)
+
             return A, Z
 
     def predict(self, X):
@@ -182,14 +268,13 @@ class NN:
     def update_params(self):
         # for NN work with n layers, there are n weights indexed from 0 to n-1
         for i in range(self.layers):
-            temp = self.W[i][0, 0]
             self.W[i] -= self.dW[i] * self.lr
             self.b[i] -= self.db[i] * self.lr
 
     def activate(self, activation, **kwargs):
         return activation(self, **kwargs)
 
-    def ReLu(self, X, gradient=False):
+    def ReLu(self, X, y=None, gradient=False):
         """
         Relu activation
         Returns either forward pass value or gradient
@@ -208,7 +293,7 @@ class NN:
         else:
             return np.where(X > 0, X, 0)
 
-    def sigmoid(self, X, gradient=False):
+    def sigmoid(self, X, y=None, gradient=False):
         """
         Sigmoid activation
         Returns either forward pass value or gradient
@@ -227,7 +312,7 @@ class NN:
         else:
             return X * (1 - X)
 
-    def softmax(self, X, gradient=False):
+    def softmax(self, X, y=None, gradient=False):
         """
         softmax activation
 
@@ -241,41 +326,54 @@ class NN:
 
         """
         if gradient:
-            return X - self.y
+            return X - y
         else:
             shiftX = X - X.max(axis=1, keepdims=True)
             exps = np.exp(shiftX)
             return exps / np.sum(exps, axis=1, keepdims=True)
 
-    def xe_loss(self, X, y):
+    def xe_loss(self, X, y, m):
         """
         Calculate cross entropy loss.
-        Normalises by batch size.
+
+        Parameters
+        ----------
+        X : array, features
+        y : array, one-hot encoded labels
+        m : int
+            batch size
 
         If L1 or L2 regularization is used, we return two values: it is helpful
         to keep track of loss without regularization to compare betweed models
         with different regularization or different parameters.
         """
         epsilon = 0.0000000001
-        if self.regularization is None:
+        if self.regularization == "L1":
             loss_reg = 0
-        else:
-            # add terms for weights in each layer
             for i in range(self.layers):
-                loss_reg = 0
-                loss_reg += 0.5 * (self.W[i] ** 2).sum() / self.batch_size
-        xel = -(y * np.log(X + epsilon)).sum() / self.batch_size
+                loss_reg += (
+                    self.reg_alpha
+                    * np.where(self.W[i] > 0, self.W[i], -self.W[i]).sum()
+                )
+        elif self.regularization == "L2":
+            # add terms for weights in each layer
+            loss_reg = 0
+            for i in range(self.layers):
+                loss_reg += self.reg_alpha * 0.5 * (self.W[i] ** 2).sum() / m
+        else:
+            loss_reg = 0
+        xel = -(y * np.log(X + epsilon)).sum() / m
         return xel, xel + loss_reg
 
-    def dZ(self, l):
+    def de_Z(self, l, y):
         """
         This returns dZ/dA in the general case, but for the final layer returns
         dL/dA where L is the loss function
 
         l is an integer representing layer
         """
-        if layer == self.layers:
-            return self.softmax(X=self.A[self.layers], gradient=True)
+        if l == self.layers - 1:
+            return self.softmax(X=self.A[self.layers], y=y, gradient=True)
         else:
             dZ = (
                 self.activate(self.activations[l], X=self.Z[l], gradient=True)
@@ -283,14 +381,24 @@ class NN:
             )
             return dZ
 
-    def dW(self, l):
+    def de_W(self, l, m):
         """Returns dW_l/dZ_l"""
-        return (self.A[l].T @ self.dZ[l]) / self.batch_size
+        if self.regularization == "L1":
+            reg = self.reg_alpha * np.where(self.W[l] > 0, 1, -1)
+        elif self.regularization == "L2":
+            reg = self.reg_alpha * self.W[l]
+        else:
+            reg = 0
+        return (self.A[l].T @ self.dZ[l]) / m + reg
 
-    def db(self, l):
+    def de_b(self, l, m):
         """Returns db_l/dZ_l"""
-        return self.dZ[l].sum(axis=0, keepdims=True) / self.batch_size
 
-    def dA(self, l):
+        return self.dZ[l].sum(axis=0, keepdims=True) / m
+
+    def de_A(self, l):
         """Returns dA_l/dZ_l"""
         return self.dZ[l] @ self.W[l].T
+
+    def plot_error(self):
+        plt.plot(self.L)
